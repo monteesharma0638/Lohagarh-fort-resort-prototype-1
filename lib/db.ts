e460da08b29e6fde680198b1422ec3e54ec45647
Monty Sharma <monteesharma0638@gmail.com>
2026-03-14 23:02:18 +0530

    fixed the db bug

--- lib/db.ts
+++ lib/db.ts
@@ -2,11 +2,14 @@ import Properties from "@/models/Properties";
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
+const DB_NAME = process.env.DB_NAME!;
 
 export async function connectDB() {
   if (mongoose.connection.readyState >= 1) return;
 
-  await mongoose.connect(MONGODB_URI);
+  await mongoose.connect(MONGODB_URI, {
+    dbName: DB_NAME
+  });
 }
 
 export async function getHotel(id: string) {
